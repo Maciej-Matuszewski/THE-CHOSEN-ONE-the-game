0585906975e7363584c0856d96b1b4d8130a44de
Gr3mlin106 <maciej.matuszewski@me.com>
2015-11-15 15:46:41 +0100

    Initial Commit

---
+++ THE CHOSEN ONE: the game/GameViewController.swift
@@ -0,0 +1,53 @@
+//
+//  GameViewController.swift
+//  THE CHOSEN ONE: the game
+//
+//  Created by Maciej Matuszewski on 15.11.2015.
+//  Copyright (c) 2015 Maciej Matuszewski. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let scene = GameScene(fileNamed:"GameScene") {
+            // Configure the view.
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            skView.presentScene(scene)
+        }
+    }
+
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return .AllButUpsideDown
+        } else {
+            return .All
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
---
+++ THE CHOSEN ONE: the gameTests/THE_CHOSEN_ONE__the_gameTests.swift
@@ -0,0 +1,36 @@
+//
+//  THE_CHOSEN_ONE__the_gameTests.swift
+//  THE CHOSEN ONE: the gameTests
+//
+//  Created by Maciej Matuszewski on 15.11.2015.
+//  Copyright © 2015 Maciej Matuszewski. All rights reserved.
+//
+
+import XCTest
+@testable import THE_CHOSEN_ONE__the_game
+
+class THE_CHOSEN_ONE__the_gameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
